99bab8117fc4a32446519c72cf6fab973158ebaa
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-08 18:48:40 -0300

    Add mongoose.

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,9 @@ routes.get('/', (request, response) => {
   return response.send('Hello world');
 });
 
+routes.post('/devs', (request, respose) => {
+  console.log(request.body);
+  return respose.send({ ok: true });
+});
+
 module.exports = routes;
