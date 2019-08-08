a84b75601f35aa6d24fc749f1828bb22b94c4590
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-08 19:05:02 -0300

    Create user.

--- src/controllers/Dev.js
+++ src/controllers/Dev.js
@@ -1,12 +1,29 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
+
 module.exports = {
   async store(request, response) {
     const { username } = request.body;
 
-    const apiresponse = await axios.get(
+    const userExists = await Dev.findOne({ user: username });
+
+    if (userExists) {
+      console.log(`Usuário ${username} já cadastrado!`);
+      return response.json(userExists);
+    }
+    const apiResponse = await axios.get(
       `https://api.github.com/users/${username}`
     );
 
-    return response.json(apiresponse.data);
+    const { name, bio, avatar_url: avatar } = apiresponse.data;
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar
+    });
+
+    console.log(`Usuário ${username} cadastrado com sucesso!`);
+    return response.json(dev);
   }
 };
