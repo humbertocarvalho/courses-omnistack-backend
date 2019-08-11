fa0427a4efba2a8b56d71f8ca019a6dfabbf4330
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-11 20:13:25 -0300

    Add match feature.

--- src/controllers/Like.js
+++ src/controllers/Like.js
@@ -12,12 +12,21 @@ module.exports = {
     }
 
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log('Deu match');
+      const loggedSocket = request.connectedUsers[user];
+      const targetSocket = request.connectedUsers[devId];
+
+      if (loggedSocket) {
+        request.io.to(loggedSocket).emit('match', targetDev);
+      }
+
+      if (targetSocket) {
+        request.io.to(targetSocket).emit('match', loggedDev);
+      }
     }
 
     loggedDev.likes.push(targetDev._id);
-    await loggedDev.save();
 
+    await loggedDev.save();
     return response.json(loggedDev);
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const cors = require('cors');
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
+  connectedUsers[user] = socket.id;
+});
 
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@cluster0-yglru.mongodb.net/omnistack8?retryWrites=true&w=majority',
@@ -12,8 +22,15 @@ mongoose.connect(
   }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((request, response, next) => {
+  request.io = io;
+  request.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
